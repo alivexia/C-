6e9f078de8befdc71aca47ef135004c3fb1e31c0
国超 夏 <istillalive@live.cn>
2016-05-01 10:30:21 +0800

    to upper

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -1,4 +1,10 @@
 #include<iostream>
+#include<string>
+#include<cctype>
 using namespace std;
 int main() {
+	string word;
+	getline(cin, word);
+	for (auto c : word)
+		cout << (char)toupper(c);
 }
