4a47d3acedd7d771a5cb11154423d7f763e8839b
国超 夏 <istillalive@live.cn>
2016-05-03 23:14:29 +0800

    迭代器

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -1,14 +1,11 @@
 #include<iostream>
-#include<vector>
+#include<string>
 using namespace std;
 int main() {
-	vector<int> grades(11, 0);
-	int scores;
-	while(cin >> scores)
-		if (scores <= 100)
-		{
-			++grades[scores / 10];
-		}						  
-	for (auto i : grades)
-		cout << i <<",";
+	string s("Some string");
+	if (s.begin() != s.end()) {
+		auto it = s.begin();
+		*it = tolower(*it);
+	}
+	cout << s;
 }
